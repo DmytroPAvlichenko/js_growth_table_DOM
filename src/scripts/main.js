18bdc8889977fe87ca0e572bf61c89ed09a2011f
dimon <80713na@gmail.com>
2025-08-25 17:49:32 +0300

    tebel

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,8 +9,8 @@ const removeCol = document.querySelector('.remove-column');
 const appendRow = document.querySelector('.append-row');
 const appendCol = document.querySelector('.append-column');
 
-let columCount = firstRow.querySelectorAll('td').length;
-let rowCount = table.querySelectorAll('tbody tr').length;
+let columCount = 4;
+let rowCount = 4;
 
 function buttonDisabled() {
   removeCol.disabled = columCount <= 2;
